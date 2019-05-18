4ce8fbdcc1a711c6b6f5b3152bfeae458a6b080f
Alexey Kaplin <akaplin@tlwr841n.local>
2019-05-18 05:34:35 +0300

    changes to source code

--- src/ta.c
+++ src/ta.c
@@ -6,36 +6,29 @@
 
 void usage()
 {
-    fprintf(stderr, "USAGE: %s x1 x2 x3 y1 y2 y3\n", PROG_NAME);
+    fprintf(stderr, "USAGE: %s b h\nwhere b - the basis of triangle, h - it's height.\n", PROG_NAME);
 }
 
 int main(int argc, char **argv)
 {
-    int x1 = 0;
-    int x2 = 0;
-    int x3 = 0;
-    
-    int y1 = 0;
-    int y2 = 0;
-    int y3 = 0;
+    double b;
+    double h;
     
     //checking command line
     if((argc == 1) || 
-        (argc < 7) || 
-         argc > 7) {
+        (argc < 3) || 
+         argc > 3) {
         usage();
         return 1;
     }
     
     //Getting positional pagameters
-    x1 = atoi(argv[1]);
-    x2 = atoi(argv[2]);
-    x3 = atoi(argv[3]);
+    //b = atan(argv[1]);
+    //h = atan(argv[2]);
     
-    y1 = atoi(argv[4]);
-    y2 = atoi(argv[5]);
-    y3 = atoi(argv[6]);
+    sscanf(argv[1], "%lf", &b);
+    sscanf(argv[2], "%lf", &h);
     
-    fprintf(stderr, "Area of triange is %lf\n", triangle_area(x1, x2, x3,y1,y2,y3));
+    fprintf(stderr, "Area of triange is %3.2lf\n", triangle_area(b,h));
     return 0;
 }
